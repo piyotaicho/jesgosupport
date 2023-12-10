d3358382a7d29198c9e6f919528ff28c0c563039
Yasuhiro Yamamoto <mohyama@mohnet.com>
2023-12-11 01:11:50 +0900

    リサイズ更新が半端ないのでテスト環境のエラー回避

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,21 @@ process.env.VUE_APP_VERSION = require('./package.json')?.version
 
 module.exports = defineConfig({
   publicPath: './',
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    client: {
+      overlay: {
+        runtimeErrors: (error) => {
+          const ignoreErrors = [
+            'ResizeObserver loop limit exceeded',
+            'ResizeObserver loop completed with undelivered notifications.'
+          ]
+          if (ignoreErrors.includes(error.message)) {
+            return false
+          }
+          return true
+        }
+      }
+    }
+  }
 })
